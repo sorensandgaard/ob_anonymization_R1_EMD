4edddc9088d1ab6dd852bd528c8c8cee49b75c13
Søren Vestergren Sandgaard <soren.sandgaard@sund.ku.dk>
2025-03-11 11:06:37 +0100

    Developed EMD calculation

--- earth_movers_distance.R
+++ earth_movers_distance.R
@@ -1,29 +1,34 @@
 #!/usr/bin/env Rscript
 library("tidyverse")
 library("Seurat")
-library("cyCombine")
-library("parallel")
 library("Matrix")
+library("EMDomics")
 
+rm(list=ls())
 
 args = commandArgs(trailingOnly=TRUE)
 case_pos <- args[1]
 ctrl_pos <- args[2]
 outdir <- args[3]
 
-# case_pos = out/data/D1/default/init_align/P1/param_0/second_align/M1/param_0/D1_case.rds
-# ctrl_pos = out/data/D1/default/init_align/P1/param_0/second_align/M1/param_0/D1_ctrl.rds
-# outdir = out/data/D1/default/init_align/P1/param_0/second_align/M1/param_0/metrics/m1/default/D1.somefile.txt
+
+# setwd("/home/projects/dtu_00062/people/sorsan/ob_anonymization_dataloss")
+# case_pos <- "out/data/D1/default/init_align/P1/param_0/second_align/M1/param_0/D1_case.rds"
+# ctrl_pos <- "out/data/D1/default/init_align/P1/param_0/second_align/M1/param_0/D1_ctrl.rds"
+# outdir <- "out/data/D1/default/init_align/P1/param_0/second_align/M1/param_0/metrics/m1/default/D1.somefile.txt"
 
 case_obj <- readRDS(case_pos)
 ctrl_obj <- readRDS(ctrl_pos)
 
 # Do something - write earth movers distance
 
-# Convert Seurat objects to expression matrices
-expr_orig <- case_obj[["RNA"]]$counts
-expr_anon <- case_obj[["RNA"]]$counts
+# Normalize
+case_obj <- NormalizeData(case_obj)
+ctrl_obj <- NormalizeData(ctrl_obj)
 
+# Convert Seurat objects to expression matrices
+expr_orig <- case_obj[["RNA"]]$data
+expr_anon <- case_obj[["RNA"]]$data
 
 # Check genelists are identical and save genelist and genecount
 genelist_orig <- rownames(expr_orig)
@@ -33,58 +38,25 @@ try(if(!identical(genelist_orig,genelist_anon)) stop("Genelists are not identica
 genelist <- genelist_orig
 rm(genelist_orig,genelist_anon)
 
-genecount <- length(genelist)
+# Keep only genes that are non-zero in at least one matrix
+non_zero_anon <- rowSums(expr_anon > 0) > 0
+non_zero_orig <- rowSums(expr_orig > 0) > 0
+common_genes <- non_zero_anon & non_zero_orig
 
-# Get cellcounts for each matrix
-cellcount_orig <- dim(expr_orig)[2]
-cellcount_anon <- dim(expr_anon)[2]
+expr_anon <- as.matrix(expr_anon[common_genes, ])
+expr_orig <- as.matrix(expr_orig[common_genes, ])
 
-# Create clusters for parallel processing
-cl <- makeCluster(detectCores())
-print("Number of clusters")
-print(length(cl))
-clusterExport(cl, c("expr_orig","expr_anon","genelist","genecount","cellcount_orig","cellcount_anon","compute_emd"))
+# Combine the expression matrices
+combined_matrix <- cbind(expr_anon, expr_orig)
+colnames(combined_matrix) <- paste("sample", 1:ncol(combined_matrix), sep="")
 
-# Ensure proper initialization of clusters
-tmp <- parLapply(cl = cl, X = 1:length(cl),function(i){
-  try(expr_orig[1,1])
-  TRUE
-})
-rm(tmp)
+# Create labels vector
+labels <- c(rep("Group1", ncol(expr_anon)), rep("Group2", ncol(expr_orig)))
+names(labels) <- colnames(combined_matrix)
 
-# Run parallel script for EMD calculation
-emds <- parLapply(cl = cl,X = 1:genecount, function(i){
-  # Gene expression for i'th gene
-  tmp_1 <- expr_orig[i,]
-  tmp_2 <- expr_anon[i,]
-  
-  # Save expression as batches
-  tmp_df <- data.frame(
-    exp = c(tmp_1,tmp_2),
-    batch = rep(c("orig","anon"),times=c(cellcount_orig,cellcount_anon)),
-    label = 1
-  )
-  
-  # Compute EMD
-  tmp <- compute_emd(tmp_df,markers = "exp", cell_col = 'label')
-  
-  # Save EMD to dataframe
-  tmp_emd <- data.frame(tmp)[1,2]
-  data.frame(gene = genelist[i],
-             emd = tmp_emd,
-             read_count_orig = sum(tmp_1),
-             read_count_anon = sum(tmp_2))
-})
+# Calculate EMD
+emd_results <- calculate_emd(combined_matrix, labels,nperm = 2,parallel = F)
 
-# Stop parallelization
-stopCluster(cl = cl)
-rm(cl)
+emd_results <- emd_results$emd
 
-# Create dataframe of EMDs
-emds <- Reduce("rbind", emds)
-rownames(emds) <- emds$gene
-emds <- emds %>% 
-  mutate(difference = read_count_orig - read_count_anon)
-
-### Write EMD dataframe to file ###
-write.table(emds, file = outdir, sep="\t")
+outdir
