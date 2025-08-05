2eb0051912fbfdb134a6ea8f5fce3ee7aa168930
Søren Vestergren Sandgaard <soren.sandgaard@sund.ku.dk>
2025-08-05 14:28:18 +0200

    Fixed output path

--- earth_movers_distance.R
+++ earth_movers_distance.R
@@ -92,4 +92,4 @@ json_obj <- list(
 )
 
 print("Writing JSON")
-write_json(json_obj, "/home/projects/dtu_00062/people/sorsan/ob_anonymization_benchmark/m1.json, pretty = TRUE, auto_unbox = TRUE)
+write_json(json_obj, outdir, pretty = TRUE, auto_unbox = TRUE)
